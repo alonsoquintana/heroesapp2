db3dd1ce38346e429cb06d000f533beb4d952c4f
Alonso Paucar <iaquintanapa@gmail.com>
2022-11-16 21:37:51 -0500

    El la parte que no funcionaba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +16,9 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
