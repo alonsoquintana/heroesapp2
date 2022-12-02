3171f446aed99ff6557f50dc731274b843b514aa
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-01 21:16:14 -0500

    Fin parte 2

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -12,6 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSelecionado: Heroe | undefined;
 
   constructor( private heroesService: HeroesService ) { }
 
@@ -21,9 +23,23 @@ export class BuscarComponent implements OnInit {
 
   buscando() {
 
-    this.heroesService.getHeroes()
+    this.heroesService.getSugerencias( this.termino.trim() )
       .subscribe( heroes => this.heroes = heroes );
 
+  }
+
+  opcionSelecionada( event: MatAutocompleteSelectedEvent) {
+
+    if(!event.option.value) {
+      this.heroeSelecionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId( heroe.id! )
+        .subscribe( heroe => this.heroeSelecionado = heroe );
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,14 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
-
-  private baseUrl: string = environment.baseUrl;
 
   constructor( private http: HttpClient ) { }
 
@@ -19,7 +16,13 @@ export class HeroesService {
   }
 
   getHeroePorId( id: string ):Observable<Heroe> {
-    return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`);
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+  }
+
+  getSugerencias( termino: string ):Observable<Heroe[]> {
+
+    return this.http.get<Heroe[]>(`http://localhost:3000/heroes?q=${ termino }&_limit=6`);
+
   }
 
 }
